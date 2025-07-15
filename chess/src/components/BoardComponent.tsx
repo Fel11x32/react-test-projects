8a29d3544555c7591f7e4c6eba3a9e06217ca4ac
Narizhnyi Vladyslav <vulfiontop@gmail.com>
2025-07-15 22:07:46 +0200

    add selected cell function

--- chess/src/components/BoardComponent.tsx
+++ chess/src/components/BoardComponent.tsx
@@ -1,23 +1,39 @@
-import React from "react";
-import type { FC } from "react";
-import type { Board } from "../models/Board"
-import CellComponent from "./CellComponent";
+import React, { useState } from 'react'
+import type { FC } from 'react'
+import type { Board } from '../models/Board'
+import CellComponent from './CellComponent'
+import type { Cell } from '../models/Cell'
 
 interface BoardProps {
-	board: Board;
-	setBoard: (board: Board) => void;
+	board: Board
+	setBoard: (board: Board) => void
 }
 
-const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+	const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+	const click = (cell: Cell) => {
+		if (cell.figure) {
+			setSelectedCell(cell)
+		}
+	}
+
 	return (
 		<div className='board'>
-			{board.cells.map((row, index) => 
+			{board.cells.map((row, index) => (
 				<React.Fragment key={index}>
-					{row.map(cell => 
-						<CellComponent cell={cell} key={cell.id}/>
-					)}
+					{row.map(cell => (
+						<CellComponent
+							onClick={click}
+							cell={cell}
+							key={cell.id}
+							selected={
+								cell.x === selectedCell?.x && cell.y === selectedCell?.y
+							}
+						/>
+					))}
 				</React.Fragment>
-			)}
+			))}
 		</div>
 	)
 }
--- chess/src/components/CellComponent.tsx
+++ chess/src/components/CellComponent.tsx
@@ -1,14 +1,19 @@
-import type { FC } from "react"
-import type { Cell } from "../models/Cell"
+import type { FC } from 'react'
+import type { Cell } from '../models/Cell'
 
 interface CellProps {
 	cell: Cell
+	selected: boolean
+	onClick: (cell: Cell) => void
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, onClick }) => {
 	return (
-		<div className={['cell', cell.color].join(' ')}>
-			{cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
+		<div
+			onClick={() => onClick(cell)}
+			className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+		>
+			{cell.figure?.logo && <img src={cell.figure.logo} alt='' />}
 		</div>
 	)
 }
